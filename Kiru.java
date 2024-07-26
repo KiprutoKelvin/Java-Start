6204a94795ab755a0bda2ef1ebd56a209cdbe5a0
iamkipruto <ikruruto@gmail.com>
2024-07-26 10:45:09 +0300

    Learing Data Types in java

---
+++ Kiru.java
@@ -0,0 +1,53 @@
+class Kiru
+{
+    public static void main(String args[])
+    {
+        // byte bt = 127;
+        // short sh = 558;
+        // long l = 5854l;
+        // char c = 'k'; //literals
+        // boolean b = true;
+
+        int num1 = 7;
+        int num2 = 9;
+        int result = num1 + num2;
+        int result1 = num1 - num2;
+        int result2 = num1 * num2;
+        float result3 = (num1 / num2);
+        int result4 = num1 % num2;
+        num1 *=3;
+        
+        // float marks = 6.5f;
+        // double d = 5.8;
+
+        // System.out.println("Hi Kipruto Kiru");
+        // System.out.println(8 - 2);
+        // System.out.println(3 + 5);
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(num1);
+        // System.out.println(marks);
+
+        // byte b = 125;
+        // int a = 257;
+        // byte k = (byte) a;
+
+        // float f = 5.6f;
+        // int t = (int) f;
+
+        // System.out.println(k);
+        // System.out.println(t);
+
+        // Type promotion
+        // byte d = 10;
+        // byte e = 30;
+         
+        //  int result = d * e;
+
+        //  System.out.println(result);
+    }
+}
+ 
