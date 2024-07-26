a17aa3ca72caf4bfdef5c6caed8e57512d3a4a17
iamkipruto <ikruruto@gmail.com>
2024-07-26 10:47:30 +0300

    Learning Double in java

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo {
+
+    public static void main(String[] args) {
+        
+        double x = 8.8;
+        double y = 9.8;
+
+        // int a = 8;
+        // int b 
+
+        boolean result = x >= y;
+
+        System.out.println(result);
+    }
+}
