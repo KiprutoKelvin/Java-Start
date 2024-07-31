656cb150f0f0457ac8b70a64bc7449871eac86d6
iamkipruto <ikruruto@gmail.com>
2024-07-31 09:14:15 +0300

    Creating dynamic Arrays

---
+++ Arrays.java
@@ -0,0 +1,20 @@
+public class Arrays {
+
+    public static void main(String[] args) {
+        // int nums[] = {3,7,2,4};
+        // nums[1] = 6;
+
+        // Making it dynamic
+        int nums[] = new int [4];
+        nums[0] = 4;
+        nums[1] = 8;
+        nums[2] = 9;
+        nums[3] = 1;
+
+        for(int i=0;i<=3;i++)
+        {
+            System.out.println(nums[i]);
+        }
+  
+    }
+}
