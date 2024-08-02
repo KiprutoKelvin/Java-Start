1a0473e7d18ac6f9f13ad684a2305803c676bafa
iamkipruto <ikruruto@gmail.com>
2024-08-02 21:54:49 +0300

    Creatinng Arrays of objects entering manually

--- JaggedArray.java
+++ JaggedArray.java
@@ -30,20 +30,54 @@
 
 
 // Arrays of Objects
+class Student
+{
+    int rollno;
+    String name;
+    int marks;
+}
+
 public class JaggedArray {
 
-    public static void main(String[] args) {
-        int nums[] = new int[6];
+    public static void main(String[] args) 
+    {
+        
+        Student s1 = new Student();
+        s1.rollno = 1;
+        s1.name = "Kipruto";
+        s1.marks = 89;
 
-        nums[0] = 4;
-        nums[1] = 6;
-        nums[2] = 8;
-        nums[3] = 7;
+        Student s2 = new Student();
+        s2.rollno = 1;
+        s2.name = "Kiptogom";
+        s2.marks = 62;
 
-        for(int i=0;i<6;i++)
+        Student s3 = new Student();
+        s3.rollno = 1;
+        s3.name = "Kipsegenen";
+        s3.marks = 97;
+
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for(int i=0;i<students.length;i++)
         {
-            System.out.println(nums[i]);
+            System.out.println(students[i].name + " : " + students[i].marks);
         }
+
+        // int nums[] = new int[4];
+
+        // nums[0] = 4;
+        // nums[1] = 6;
+        // nums[2] = 8;
+        // nums[3] = 7;
+
+        // for(int i=0;i<nums.length;i++) //i<7 -Exception error
+        // {
+        //     System.out.println(nums[i]);
+        // }
     }
 }
 
