babd852c3a16b66002d59a2c6cf4849482ca66a5
iamkipruto <ikruruto@gmail.com>
2024-08-03 12:30:27 +0300

    Used string buffer

--- Strings.java
+++ Strings.java
@@ -5,19 +5,27 @@ public class Strings {
 
     public static void main(String[] args) {
         // String name = new String("Kiru");
-        String name = "Kiru";
-        name = name + " Kipruto";
+        // String name = "Kiru";
+        // name = name + " Kipruto";
 
-        System.out.println("Salut " + name);
+        // System.out.println("Salut " + name);
 
-        String s1 = "Neet";
-        String s2 = "Neet";
+        // String s1 = "Neet";
+        // String s2 = "Neet";
 
-        System.out.println(s1 == s2); //String constant pool
+        // System.out.println(s1 == s2); //String constant pool
 
         // System.out.println("Guten Mogen " + name);
-        // // System.out.println(name.hashCode());
+        // System.out.println(name.hashCode());
         // System.out.println(name.charAt(1));
         // System.out.println(name.concat(" Kipruto"));
+
+
+        // String Buffer
+        StringBuffer sb = new StringBuffer("Kipruto");
+        System.out.println(sb.length()); 
+        sb.append(" Kelvin");
+        System.out.println(sb);
+
     }
 }
