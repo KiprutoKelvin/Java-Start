c2f2ef52fad846e24b08eea696246b59715bc0a8
iamkipruto <ikruruto@gmail.com>
2024-08-03 08:54:48 +0300

    Crating strings and working on methods in java

---
+++ Strings.java
@@ -0,0 +1,15 @@
+/**
+ * Strings
+ */
+public class Strings {
+
+    public static void main(String[] args) {
+        String name = new String("Kiru");
+        // String name = "Kiru";
+
+        System.out.println("Guten Mogen " + name);
+        // System.out.println(name.hashCode());
+        System.out.println(name.charAt(1));
+        System.out.println(name.concat(" Kipruto"));
+    }
+}
