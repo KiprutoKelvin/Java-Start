7c4aeeddf4dffd993c5aad450608ac2f283993c4
iamkipruto <ikruruto@gmail.com>
2024-08-03 09:09:57 +0300

    Learning String pool constant

--- Strings.java
+++ Strings.java
@@ -4,12 +4,20 @@
 public class Strings {
 
     public static void main(String[] args) {
-        String name = new String("Kiru");
-        // String name = "Kiru";
+        // String name = new String("Kiru");
+        String name = "Kiru";
+        name = name + " Kipruto";
 
-        System.out.println("Guten Mogen " + name);
-        // System.out.println(name.hashCode());
-        System.out.println(name.charAt(1));
-        System.out.println(name.concat(" Kipruto"));
+        System.out.println("Salut " + name);
+
+        String s1 = "Neet";
+        String s2 = "Neet";
+
+        System.out.println(s1 == s2); //String constant pool
+
+        // System.out.println("Guten Mogen " + name);
+        // // System.out.println(name.hashCode());
+        // System.out.println(name.charAt(1));
+        // System.out.println(name.concat(" Kipruto"));
     }
 }
