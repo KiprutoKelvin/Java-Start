941dceefa35a9e06e2039a4ae572626ccbdd46aa
iamkipruto <ikruruto@gmail.com>
2024-08-06 16:37:41 +0300

    Creating Anonymous Object

--- Demo.java
+++ Demo.java
@@ -1,15 +1,56 @@
-public class Demo {
+// public class Demo {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
         
-        double x = 8.8;
-        double y = 9.8;
+//         double x = 8.8;
+//         double y = 9.8;
 
-        // int a = 8;
-        // int b 
+//         // int a = 8;
+//         // int b 
 
-        boolean result = x >= y;
+//         boolean result = x >= y;
 
-        System.out.println(result);
+//         System.out.println(result);
+//     }
+// }
+
+/** Naming Convention **/ 
+
+// camelCasing
+// Class and Interface start with capital - Capital 
+// variable and methods  e.g marks, show()
+// CONSTANTS - PIE, BRAND
+
+
+/**
+ * Demo Anonymous Object
+ */
+
+ class A
+ {
+    public A()
+    {
+        System.out.println("Object Created");
     }
+    public void show()
+    {
+        System.out.println("In a Show");
+    }
+ }
+
+public class Demo 
+{
+    public static void main(String[] args) {
+        // int marks;
+        // marks = 99;
+        
+        // A obj;
+        // obj = new A();
+        // obj.show();
+
+        new A(); // Anonymous Object - you cant reuse them
+        new A().show();
+    }
+
+    
 }
