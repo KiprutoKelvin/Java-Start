9ece0ceee1777609c2d2b5971891a778a9d227f3
iamkipruto <ikruruto@gmail.com>
2024-08-07 07:52:11 +0300

    Working on Multilevel Inheritance

--- Inheritance.java
+++ Inheritance.java
@@ -17,14 +17,15 @@
 public class Inheritance {
     public static void main(String[] args) {
         // Calc obj = new Calc();
-        AdvCalc obj = new AdvCalc();
+        // AdvCalc obj = new AdvCalc();
+        VeryAdvCalc obj = new VeryAdvCalc();
         int r1 = obj.add(7, 3);
         int r2 = obj.sub(15, 9);
         int r3 = obj.multi(5, 7);
         int r4 = obj.div(16, 5);
+        double r5 = obj.power(4, 4);
 
-
-        System.out.println(r1 + " " + r2 + " " +  r3 + " " + r4);
+        System.out.println(r1 + " " + r2 + " " +  r3 + " " + r4 + " " + r5);
     }
 
     
