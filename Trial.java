087aafda3f099895107db2c2596bfca5a590df79
iamkipruto <ikruruto@gmail.com>
2024-08-09 20:25:23 +0300

    Learning packages

---
+++ Trial.java
@@ -0,0 +1,25 @@
+
+/**
+ * Trial packages
+ */
+
+// import tools.Calc;
+// import tools.AdvCalc;
+// import java.util.ArrayList;
+// import java.lang.*;  // Always there even if you dont mention
+
+import tools.*;
+
+public class Trial {
+    public static void main(String[] args) {
+        // ArrayList list = new ArrayList();
+
+        Calc obj = new Calc();
+        AdvCalc obj1 = new AdvCalc();
+
+        System.out.println();
+
+
+    }
+    
+}
