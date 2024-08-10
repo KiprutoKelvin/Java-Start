96b161f48191fe55591009cd7fb04b4e40987b76
iamkipruto <ikruruto@gmail.com>
2024-08-10 10:39:59 +0300

    Learning Access Modifiers

---
+++ other/Ac.java
@@ -0,0 +1,20 @@
+/**
+ * Access Modifiers
+ */
+package other;
+
+public class Ac {
+    public int marks = 60;
+
+   
+}
+/*
+
+                                  Public   Private  Protected    Default
+Same class                          Yes      No       Yes          Yes
+Same package subclass               Yes      No       Yes          Yes
+Same package non-subclass           Yes      No       Yes          Yes
+Different package subclass          Yes      No       Yes          No
+Different package non-subclass      Yes      No       No           No
+
+                  */
