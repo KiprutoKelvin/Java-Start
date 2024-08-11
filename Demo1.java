8dca13c807ca73046fd38b6c49621b91e410a7fd
iamkipruto <ikruruto@gmail.com>
2024-08-11 08:35:22 +0300

    Learning Access Modifiers

---
+++ Demo1.java
@@ -0,0 +1,14 @@
+import other.*;
+
+/**
+ * Demo1
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        Ac obj = new Ac();
+        System.out.println(obj.marks);
+
+
+    }
+}
