e67dfea447d9d66875a10dbf786457813fec5a32
iamkipruto <ikruruto@gmail.com>
2024-08-11 08:36:21 +0300

    Learning Polymorphism and Dynamic Method Dispatch

---
+++ Demo2.java
@@ -0,0 +1,22 @@
+/**
+ * Demo2 Dynamic Method Dispatch
+ */
+class Ap
+{
+    public void show()
+    {
+        System.out.println("In a Show");
+    }
+}
+class Bp extends Ap
+{
+
+}
+class Cp extends Ap
+{
+
+}
+public class Demo2 {
+
+    
+}
