080f377c4373c182d848916f0d291911abd16abd
iamkipruto <ikruruto@gmail.com>
2024-08-12 17:11:58 +0300

    Working on Object class, equal, toString and hashcode

--- Object.java
+++ Object.java
@@ -11,7 +11,60 @@ class Calculator{
         return r;
     }
 }
+class Laptop
+{
+    String model;
+    int price;
 
+    // public String toString()
+    // {
+    //     // return "Salut";
+    //     return model + " : " + price;
+    // }
+    // public boolean equals(Laptop that)
+    // {
+    //     // if(this.model.equals(that.model) && this.price == that.price)
+    //     //     return true;
+    //     // else
+    //     //     return false;
+
+    //     return this.model.equals(that.model) && this.price == that.price;
+        
+    // }
+
+    @Override  // Annotations
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((model == null) ? 0 : model.hashCode());
+        result = prime * result + price;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Laptop [model=" + model + ", price=" + price + "]";
+    }
+
+    @Override
+    public boolean equals(java.lang.Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Laptop other = (Laptop) obj;
+        if (model == null) {
+            if (other.model != null)
+                return false;
+        } else if (!model.equals(other.model))
+            return false;
+        if (price != other.price)
+            return false;
+        return true;
+    } 
+}
 public class Object {
 
     public static void main(String[] args) {
@@ -25,5 +78,20 @@ public class Object {
         // int result = num1 + num2;
 
         System.out.println(result);
+
+        Laptop obj = new Laptop();
+        obj.model = "Lenovo Yoga";
+        obj.price = 1200;
+
+        Laptop obj1 = new Laptop();
+        obj1.model = "Lenovo Yoga";
+        obj1.price = 1200;
+
+        // boolean result1 = obj == obj1;
+        boolean result1 = obj.equals(obj1);
+
+        // System.out.println(obj);
+        System.out.println(obj.toString());
+        System.out.println(result1);
     }
 }
