47cb5ea35fe57e5656bc99c0a2c287d09c34e0b3
iamkipruto <ikruruto@gmail.com>
2024-08-14 10:46:38 +0300

    Working on Anonymous Inner Class

--- Demo3.java
+++ Demo3.java
@@ -2,34 +2,68 @@
  * Demo3 
  * Inner Class
  */
-class Ai
-{
-    int age;
+// class Ai
+// {
+//     int age;
 
+//     public void show()
+//     {
+//         System.out.println("In Show");
+//     }
+
+//     // class Bi
+//     static class Bi
+//     {
+//         public void config()
+//         {
+//             System.out.println("In Config");
+//         }
+//     }
+
+// }
+// public class Demo3 
+// {
+//     public static void main(String[] args) {
+//         Ai obj = new Ai();
+//         obj.show();
+
+//         // Ai.Bi obj1 = obj.new Bi();
+//         Ai.Bi obj1 = new Ai.Bi();
+//         obj1.config();
+//     }
+
+    
+// }
+
+/*
+ * Anonymous Inner Class
+ */
+class An
+{
     public void show()
     {
-        System.out.println("In Show");
+        System.out.println("In An Show");
     }
-
-    // class Bi
-    static class Bi
-    {
-        public void config()
-        {
-            System.out.println("In Config");
-        }
-    }
-
 }
+// class Bn extends An
+// {
+//     public void show()
+//     {
+//         System.out.println("In Bn Show");
+//     }
+// }
 public class Demo3 
 {
     public static void main(String[] args) {
-        Ai obj = new Ai();
+        An obj = new An()
+        // Anonymous Inner Class
+        {
+            public void show()
+            {
+                System.out.println("In new Show");
+            }
+        };
         obj.show();
-
-        // Ai.Bi obj1 = obj.new Bi();
-        Ai.Bi obj1 = new Ai.Bi();
-        obj1.config();
     }
 
     
