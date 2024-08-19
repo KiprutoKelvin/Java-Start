735e86e9dd20d140cef8c568b1bb4fd5100cd386
iamkipruto <ikruruto@gmail.com>
2024-08-19 09:23:19 +0300

    Working on Enum if and switch statements

--- Enum.java
+++ Enum.java
@@ -7,15 +7,44 @@ enum Status{
 public class Enum {
     public static void main(String[] args) 
     {
-        int i = 6;
-        // Status s = Status.Running;
-        Status[] ss = Status.values();
-        // System.out.println(s.ordinal());
-        // System.out.println(ss[0]);
+        // int i = 6;
+        // // Status s = Status.Running;
+        // Status[] ss = Status.values();
+        // // System.out.println(s.ordinal());
+        // // System.out.println(ss[0]);
 
-        for(Status s : ss)
-        {
-            System.out.println(s + " : " + s.ordinal());
+        // for(Status s : ss)
+        // {
+        //     System.out.println(s + " : " + s.ordinal());
+        // }
+
+        Status s = Status.Running;
+
+        switch (s) {
+            case Success:
+                System.out.println("All Good");
+                break;
+
+            case Failed:
+                System.out.println("Try Again");
+                break;
+            
+            case Pending:
+                System.out.println("Please Wait");
+                break;
+
+            default:
+                System.out.println("Done");
+                break;
         }
+
+        // if(s == Status.Running)
+        //     System.out.println("All Good");
+        // else if(s == Status.Failed)
+        //     System.out.println("Try Again");
+        // else if (s == Status.Pending)
+        //     System.out.println("Please Wait");
+        // else
+        // System.out.println("Done");
     }
 }
