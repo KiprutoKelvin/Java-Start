45e1243edc911016915b8f265512c08e6db165a2
iamkipruto <ikruruto@gmail.com>
2024-08-20 13:27:09 +0300

    Working on Annotations

--- Demo1.java
+++ Demo1.java
@@ -3,12 +3,29 @@ import other.*;
 /**
  * Demo1
  */
+class A
+{
+    public void show()
+    {
+        System.out.println("In A show");
+    }
+}
+class B extends A
+{   
+    @Override
+    public void show()
+    {
+        System.out.println("In B show");
+    }
+}
 public class Demo1 {
 
-    public static void main(String[] args) {
-        Ac obj = new Ac();
-        System.out.println(obj.marks);
+    public static void main(String[] args) 
+    {
+        // Ac obj = new Ac();
+        // System.out.println(obj.marks);
 
-
+        B obj = new B();
+        obj.show();
     }
 }
