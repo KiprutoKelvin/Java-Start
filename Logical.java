e0611d6165bb2344563a35c7433009eaed72fdea
iamkipruto <ikruruto@gmail.com>
2024-08-21 13:29:27 +0300

    Working on Try Catch

--- Logical.java
+++ Logical.java
@@ -1,13 +1,33 @@
 public class Logical {
 
     public static void main(String[] args) {
-        // int x = 7;
-        // int y = 5;
-        int a = 5;
-        int b = 9;
+        // // int x = 7;
+        // // int y = 5;
+        // int a = 5;
+        // int b = 9;
 
-        // boolean result = x > y || a < b || a > 1;
-        boolean result = a > b;
-        System.out.println(!result);
+        // // boolean result = x > y || a < b || a > 1;
+        // boolean result = a > b;
+        // System.out.println(!result);
+
+/*
+ * Exception Handling
+ * Using Try Catch
+ */
+        int i = 0;
+        int j = 0;
+
+        try 
+        {
+            j = 18/i;    
+        } 
+        catch (Exception e) 
+        {
+            System.out.println("Something Went Wrong...");
+        }
+
+        System.out.println(j);
+
+        System.out.println("Bye");
     }
 }
