dada6f355df10dd8fa41af150af6242843fd19dc
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2024-10-13 14:47:43 +0300

    WOrking on Runnable

--- Threads.java
+++ Threads.java
@@ -2,46 +2,76 @@
  * Threads
  * Multiple Threads
  */
-class At extends Thread
-{
-    public void run()
-    {
-        for(int i=1;i<=100;i++)
-        {
-            System.out.println("Salut");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) { e.printStackTrace(); }
-        }
-    }
-}
-class Bt extends Thread
-{
-    public void run()
-    {
-        for(int i=1;i<=100;i++)
-        {
-            System.out.println("Bonjour");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) { e.printStackTrace(); }
-        }
-    }
-}
+// class At extends Thread
+// {
+//     public void run()
+//     {
+//         for(int i=1;i<=100;i++)
+//         {
+//             System.out.println("Salut");
+//             try {
+//                 Thread.sleep(10);
+//             } catch (InterruptedException e) { e.printStackTrace(); }
+//         }
+//     }
+// }
+// class Bt extends Thread
+// {
+//     public void run()
+//     {
+//         for(int i=1;i<=100;i++)
+//         {
+//             System.out.println("Bonjour");
+//             try {
+//                 Thread.sleep(10);
+//             } catch (InterruptedException e) { e.printStackTrace(); }
+//         }
+//     }
+// }
+// public class Threads {
+//     public static void main(String[] args) {
+//         At obj1 = new At();
+//         Bt obj2 = new Bt();
+
+//         // System.out.println(obj1.getPriority());
+//         // obj2.setPriority(Thread.MAX_PRIORITY);
+
+//         obj1.start();
+//         try {
+//             Thread.sleep(5);
+//         } catch (InterruptedException e) { e.printStackTrace(); }
+
+//         obj2.start();
+//     }
+
+    
+// }
+
+/*
+ * Implementing Using Runnable
+ */
 public class Threads {
     public static void main(String[] args) {
-        At obj1 = new At();
-        Bt obj2 = new Bt();
+        Runnable obj1 = () -> {
+                for(int i=1;i<=5;i++)
+                {
+                    System.out.println("Salut");
+                    try { Thread.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
+                }
+        };
+        Runnable obj2 = () -> {
+            for(int i=1;i<=5;i++)
+            {
+                System.out.println("Bonjour");
+                try { Thread.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
+            }
+    };
 
-        // System.out.println(obj1.getPriority());
-        // obj2.setPriority(Thread.MAX_PRIORITY);
+        Thread t1 = new Thread(obj1);
+        Thread t2 = new Thread(obj2);
 
-        obj1.start();
-        try {
-            Thread.sleep(5);
-        } catch (InterruptedException e) { e.printStackTrace(); }
-
-        obj2.start();
+        t1.start();
+        t2.start();
     }
 
     
