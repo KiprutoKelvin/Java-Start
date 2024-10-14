8c58d8ef505641ae336068330747959eed4b8dcd
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2024-10-14 17:03:53 +0300

    Learning Sychronized-Race Condition

--- Threads.java
+++ Threads.java
@@ -50,20 +50,59 @@
 /*
  * Implementing Using Runnable
  */
+// public class Threads {
+//     public static void main(String[] args) {
+//         Runnable obj1 = () -> {
+//                 for(int i=1;i<=5;i++)
+//                 {
+//                     System.out.println("Salut");
+//                     try { Thread.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
+//                 }
+//         };
+//         Runnable obj2 = () -> {
+//             for(int i=1;i<=5;i++)
+//             {
+//                 System.out.println("Bonjour");
+//                 try { Thread.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
+//             }
+//     };
+
+//         Thread t1 = new Thread(obj1);
+//         Thread t2 = new Thread(obj2);
+
+//         t1.start();
+//         t2.start();
+//     }
+
+// }
+/*
+ * Race Condition
+ */
+class Counter
+{
+    int count;
+    public synchronized void increment()
+    {
+        count++;
+    }
+}
+
 public class Threads {
-    public static void main(String[] args) {
-        Runnable obj1 = () -> {
-                for(int i=1;i<=5;i++)
+        public static void main(String[] args) throws InterruptedException {
+        Counter c = new Counter();
+
+        Runnable obj1 = () -> 
+        {
+                for(int i=1;i<=10000;i++)
                 {
-                    System.out.println("Salut");
-                    try { Thread.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
+                   c.increment(); 
                 }
         };
-        Runnable obj2 = () -> {
-            for(int i=1;i<=5;i++)
+        Runnable obj2 = () -> 
+        {
+            for(int i=1;i<=10000;i++)
             {
-                System.out.println("Bonjour");
-                try { Thread.sleep(10); } catch (InterruptedException e) { e.printStackTrace(); }
+               c.increment(); 
             }
     };
 
@@ -72,7 +111,11 @@ public class Threads {
 
         t1.start();
         t2.start();
+
+        t1.join();
+        t2.join();
+
+        System.out.println(c.count);
     }
 
-    
 }
