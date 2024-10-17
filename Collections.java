54dae8c70f6fda50332906a7aa11366b30d45386
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2024-10-17 15:50:17 +0300

    Working on List in Collections

--- Collections.java
+++ Collections.java
@@ -1,19 +1,28 @@
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class Collections {
 
     public static void main(String[] args) {
-        Collection<Integer> nums = new ArrayList<Integer>();
+    //     Collection<Integer> nums = new ArrayList<Integer>();
+    //     nums.add(6);
+    //     nums.add(5);
+    //     nums.add(9);
+    //     nums.add(7);
+
+    //     for(int n : nums)
+    //     {
+    //         System.out.println(n);
+    //     }
+    //     // System.out.println(nums);
+    // }
+    List<Integer> nums = new ArrayList<Integer>();
         nums.add(6);
         nums.add(5);
         nums.add(9);
         nums.add(7);
 
-        for(int n : nums)
-        {
-            System.out.println(n);
-        }
-        // System.out.println(nums);
+        System.out.println(nums.get(3));
     }
 }
