0edf7c62a136526f5a4aece6d5a779b2676c42c5
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2024-10-31 16:32:42 +0300

    Working on Sorted values

--- Demo4.java
+++ Demo4.java
@@ -5,13 +5,19 @@ public class Demo4 {
 
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(3,4,5,7,2,8);
-        
+
         /*It's not difficult it's just unfamiliar */
         int result = nums.stream()
                         .filter(n -> n%2==0)
                         .map(n -> n*2)
                         .reduce(0, (c,e) -> c+e);
-       
+
+        Stream<Integer> sortedValues = nums.stream()
+                        .filter(n -> n%2==0)
+                        .sorted();
+        
+        sortedValues.forEach(n -> System.out.println(n));
+        System.out.println();
         System.out.println(result);
     }
 }
