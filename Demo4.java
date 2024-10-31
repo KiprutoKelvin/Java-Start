e06645e58284667da0d5f6a5d8cbd2ea7a4387f8
KiprutoKelvin <127014004+KiprutoKelvin@users.noreply.github.com>
2024-10-31 15:53:25 +0300

    Working on Stream

--- Demo4.java
+++ Demo4.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.stream.Stream;
 import java.util.Arrays;
 public class Demo4 {
 
@@ -29,7 +30,22 @@ public class Demo4 {
         //     }
         // }
         // System.out.println(sum);
-          
-        nums.forEach(n -> System.out.println(n));
+
+        // Stream<Integer> s1 = nums.stream();
+        // Stream<Integer> s2 = s1.filter(n -> n%2==0);
+        // Stream<Integer> s3 = s2.map(n -> n*2);
+        // int result = s3.reduce(0, (c,e) -> c+e);
+
+        /*It's not difficult it's just unfamiliar */
+        int result = nums.stream()
+                        .filter(n -> n%2==0)
+                        .map(n -> n*2)
+                        .reduce(0, (c,e) -> c+e);
+       
+        System.out.println(result);
+
+        // s3.forEach(n -> System.out.println(n));
+        // s1.forEach(n -> System.out.println(n));
+       // nums.forEach(n -> System.out.println(n));
     }
 }
